78937a0d3001e8a2cf54f6e03964e795ece5fa53
Buddheesha Induwara <buddhishainduwara@gmail.com>
2022-07-02 09:54:50 +0530

    customer route server updated

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,7 @@ app.use(cors())
 
 app.use(AuthRoutes.routes()).use(AuthRoutes.allowedMethods())
 app.use(TraderRoutes.routes()).use(TraderRoutes.allowedMethods())
+app.use(TraderRoutes.routes()).use(TraderRoutes.allowedMethods())
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
