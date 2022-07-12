033d2d73f08cdc95acf22f505ff9237d3791264c
Buddheesha Induwara <it20031602@my.sliit.lk>
2022-07-12 21:14:57 +0530

    login & regiter created

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,1 +1,73 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+import React from 'react'
+
+const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(false)
+
+  const navigate = useNavigate()
+
+  const loginHandler = async (e) => {
+    e.preventDefault()
+    const user = {
+      email,
+      password,
+    }
+
+    const response = await axios.post('http://localhost:5000/auth/login', user)
+
+    localStorage.setItem('user', JSON.stringify(response.data))
+
+    if (!response.data) {
+      setError(true)
+    }
+
+    if (response.data.role === 'customer') {
+      navigate('/customer')
+    }
+
+    if (response.data.role === 'trader') {
+      navigate('/trader')
+    }
+  }
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setError(false)
+    }, 3000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [error])
+
+  return (
+    <center>
+      <h2>Login</h2>
+      <form onSubmit={loginHandler}>
+        <label>Email </label>
+        <input
+          type='text'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br /> <br />
+        <label>Password </label>
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <br />
+        <button type='submit'>Login</button>
+      </form>
+      {error && <p>Invalid Credentials</p>}
+    </center>
+  )
+}
+
+export default Login
---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,80 @@
+import React from 'react'
+
+const Register = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [role, setRole] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  const register = async (e) => {
+    e.preventDefault()
+    const user = {
+      name,
+      email,
+      role,
+      password,
+    }
+    const response = await axios.post(
+      'http://localhost:5000/auth/register',
+      user
+    )
+
+    localStorage.setItem('user', JSON.stringify(response.data))
+
+    if (response.data.role === 'customer') {
+      navigate('/customer')
+    }
+
+    if (response.data.role === 'trader') {
+      navigate('/trader')
+    }
+  }
+
+  return (
+    <center>
+      <h2>Register</h2>
+
+      <form onSubmit={(e) => register(e)}>
+        <label>Name </label>
+        <input
+          type='text'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <br />
+        <label>Email </label>
+        <input
+          type='text'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br /> <br />
+        <label htmlFor='role'>Role </label>
+        <select
+          name='role'
+          value={role}
+          onChange={(e) => setRole(e.target.value)}
+        >
+          <option defaultValue='customer'>Select User role</option>
+          <option value='customer'>Customer</option>
+          <option value='trader'>Trader</option>
+        </select>
+        <br /> <br />
+        <label>Password </label>
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <br />
+        <button type='submit'>Register</button>
+      </form>
+    </center>
+  )
+}
+
+export default Register
